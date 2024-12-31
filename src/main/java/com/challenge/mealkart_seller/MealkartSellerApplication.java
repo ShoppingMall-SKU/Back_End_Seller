559fea06c07f92b97b6b14fc67ef911e46382fda
HwanheeMoon <yuiop0218@naver.com>
2024-12-31 09:44:10 +0900

    :tada: init : 문환희: 셀러 백엔드 서버 초기 세팅

---
+++ src/main/java/com/challenge/mealkart_seller/MealkartSellerApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.mealkart_seller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealkartSellerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MealkartSellerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/challenge/mealkart_seller/MealkartSellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.mealkart_seller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealkartSellerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
